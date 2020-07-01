c3dabb71effc6125a994accc723442ce7feb61c9
boyang-liu <64985036+boyang-liu@users.noreply.github.com>
2020-07-01 22:33:08 +0200

    tessellation + vertex manipulation+animation
    tessellation + vertex manipulation+animation

--- vr_mesh_view.h
+++ vr_mesh_view.h
@@ -244,6 +244,12 @@ public:
 	void applySmoothing();
 	void tessellation(const vec3& origin, const vec3& direction);
 	void vertex_manipulate(HE_Vertex* vertex, vec3 new_pos);
+	void start_define_path(const vec3& intersection_point, const vec3& origin);
+	void end_define_path(const vec3& origin);
+	void drawpath(cgv::render::context& ctx, std::vector<vec3> path_list);
+	void do_the_animation();
+	void along_path_go();
+	void along_path_back();
 };
 
 ///@}
